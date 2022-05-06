614446d4c77dc8baa00803ee449cd59ab2adb559
Sarah <vanwars@gmail.com>
2022-05-06 09:59:10 -0500

    HW4 posted

--- course-files/homework/hw04/app.py
+++ course-files/homework/hw04/app.py
@@ -5,15 +5,12 @@ from flask_restful import Api
 from flask_cors import CORS
 from flask import render_template
 import os
-from models import db, User, ApiNavigator
-from views import bookmarks, comments, followers, following, \
-    posts, profile, stories, suggestions, post_likes
-
+from sqlalchemy import and_
+from models import db, Post, User, Following, ApiNavigator, Story
+from views import initialize_routes, get_authorized_user_ids
 
 
 app = Flask(__name__)
-
-# CORS: allows anyone from anywhere to use your API:
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_URL')
@@ -29,16 +26,7 @@ with app.app_context():
 
 
 # Initialize routes for all of your API endpoints:
-bookmarks.initialize_routes(api)
-comments.initialize_routes(api)
-followers.initialize_routes(api)
-following.initialize_routes(api)
-posts.initialize_routes(api)
-post_likes.initialize_routes(api)
-profile.initialize_routes(api)
-stories.initialize_routes(api)
-suggestions.initialize_routes(api)
-
+initialize_routes(api)
 
 # Server-side template for the homepage:
 @app.route('/')
@@ -48,11 +36,13 @@ def home():
         user=app.current_user
     )
 
+
 @app.route('/api')
+@app.route('/api/')
 def api_docs():
     navigator = ApiNavigator(app.current_user)
     return render_template(
-        'api/api-docs.html', 
+        'api/api_docs.html', 
         user=app.current_user,
         endpoints=navigator.get_endpoints(),
         url_root=request.url_root[0:-1] # trim trailing slash
